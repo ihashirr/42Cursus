958484d31ea08741f1ab9b50b143eeac4aef94c9
Malik Hashir <mhashir@lab1r4s10.42abudhabi.ae>
2023-09-26 04:02:46 +0400

    committed C05

--- as.sh
+++ as.sh
@@ -1,3 +1,13 @@
-git remote add C04 git@vogsphere.42abudhabi.ae:vogsphere/intra-uuid-0faae6a9-4884-4eb7-8bb4-6383cd121fe1-4756892-mhashir
-git fetch C04
-git merge C04/master --allow-unrelated-histories -m "commited C04"
+remot="C05"
+
+git remote add "$remot" git@vogsphere.42abudhabi.ae:vogsphere/intra-uuid-6fab5b67-09bb-4bc3-a1aa-89da637b3cd5-4756966-mhashir
+git fetch "$remot"
+git merge "$remot/master" --allow-unrelated-histories -m "commited $remot"
+mv ex* "./Piscine/$remot"
+git add .
+git commit -m "committed $remot"
+git push origin master
+
+# for i in {5..8}; do 
+#     mkdir "C0$i"       
+# done

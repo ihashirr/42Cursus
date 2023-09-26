b23348c9a4c199f88cca349ce075ed81e703af1e
Malik Hashir <mhashir@lab1r4s10.42abudhabi.ae>
2023-09-26 04:14:44 +0400

    committed Get_next_line

--- script_merge.sh
+++ script_merge.sh
@@ -1,6 +1,6 @@
 remot="Get_next_line"
-url = "git@vogsphere.42abudhabi.ae:vogsphere/intra-uuid-18285a8d-9e16-4198-a464-bee3bc4ca0a2-5201907-mhashir"
-git remote add "$remot" "$url"
+url = "vogsphere.42abudhabi.ae:vogsphere/intra-uuid-18285a8d-9e16-4198-a464-bee3bc4ca0a2-5201907-mhashir"
+git remote add "$remot" vogsphere.42abudhabi.ae:vogsphere/intra-uuid-18285a8d-9e16-4198-a464-bee3bc4ca0a2-5201907-mhashir
 git fetch "$remot"
 git merge "$remot/master" --allow-unrelated-histories -m "commited $remot"
 #mv ex* "./Piscine/$remot"
